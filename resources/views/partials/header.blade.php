01bd3980cb1c88306b8f397a1447e62ecac67cb7
SebastianoMV <sebastianovisco@gmail.com>
2022-06-24 16:45:37 +0200

    Style

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,16 +5,16 @@
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link" href="{{ route('home') }}">Home</a>
+            <a class="nav-link {{Route::currentRouteName() == 'home' ? 'active' : ''}}" href="{{ route('home') }}">Home <i class="fa-solid fa-house-user"></i></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="{{ route('movies') }}">Movies</a>
+            <a class="nav-link {{Route::currentRouteName() == 'movies' ? 'active' : ''}}" href="{{ route('movies') }}">Movies <i class="fa-solid fa-clapperboard"></i></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="{{ route('about') }}">About</a>
+            <a class="nav-link {{Route::currentRouteName() == 'about' ? 'active' : ''}}" href="{{ route('about') }}">About <i class="fa-solid fa-address-card"></i></a>
           </li>   
           <li class="nav-item">
-            <a class="nav-link" href="{{ route('contacts') }}">Contacts</a>
+            <a class="nav-link {{Route::currentRouteName() == 'contacts' ? 'active' : ''}}" href="{{ route('contacts') }}">Contacts <i class="fa-solid fa-pen"></i></a>
           </li> 
         </ul>
       </div>
