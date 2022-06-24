c2e1255527ecee9b150ee46694f064d621e4b7b4
SebastianoMV <sebastianovisco@gmail.com>
2022-06-24 16:13:17 +0200

    Minimo style card

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -3,14 +3,39 @@
 
 @section('content')
 
-  <h1>Movies</h1>
+  <div class="container d-flex flex-wrap">
 
-  @foreach($movies as $movie)
-    <div class="card">
+    @foreach($movies as $movie)
+      <div class="card text-center p-3">
     
-      {{$movie->title}}
+        <h3>{{$movie->title}}</h3>
+
+        <p>
+          Titolo originale:
+          <br>
+          {{$movie->original_title}}
+        </p>
+
+        <div class="my-3">
+          @for($i = 1; $i < 5; $i++)
+            @if($i <= (round($movie->vote) / 2))
+              <label for="star-5" class="star">
+              <i class="fas fa-star"span class="star star--gold"></span></i>
+              </label>
+            @else
+            @endif
+          @endfor
+        </div>
+
+        <p>
+          Uscita:
+          <br>
+          {{$movie->date}}
+        </p>
     
-    </div>
-  @endforeach
+      </div>
+    @endforeach
+
+ </div> 
 
 @endsection
